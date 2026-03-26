a56009eaa9bd95225be4d40c405832cd599c71da
itsyaya <yayarezai3@gmail.com>
2026-03-26 13:33:07 +0100

    feat: Initialize Spring Boot application with global exception handling,  API response DTOs, and core configurations.

---
+++ src/main/java/com/applygenie/ApplyGenieApplication.java
@@ -0,0 +1,13 @@
+package com.applygenie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplyGenieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplyGenieApplication.class, args);
+    }
+
+}
