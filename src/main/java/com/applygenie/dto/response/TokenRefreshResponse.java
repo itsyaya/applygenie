ec9c64f303df357c2d454a63ec8c22dbdeed6b22
itsyaya <yayarezai3@gmail.com>
2026-03-28 09:44:32 +0100

    feat: add DTOs for authentication and standardized error responses (AuthResponse, ErrorResponse)

--- src/main/java/com/applygenie/dto/response/TokenRefreshResponse.java
+++ src/main/java/com/applygenie/dto/response/TokenRefreshResponse.java
@@ -9,9 +9,8 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class AuthResponse {
-    private String token;
+public class TokenRefreshResponse {
+    private String accessToken;
     private String refreshToken;
-    private String email;
-    private String type = "Bearer";
+    private String tokenType = "Bearer";
 }
