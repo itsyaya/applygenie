b09229c862cab02bbf2b97f7bbf9cd834fa9d07a
itsyaya <yayarezai3@gmail.com>
2026-03-28 09:47:10 +0100

    feat: add main Spring Boot application entry point

--- src/main/java/com/applygenie/ApplyGenieApplication.java
+++ src/main/java/com/applygenie/ApplyGenieApplication.java
@@ -1,9 +1,14 @@
 package com.applygenie;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableRetry
+@EnableAsync
 public class ApplyGenieApplication {
 
     public static void main(String[] args) {
