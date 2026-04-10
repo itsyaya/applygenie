0d146eb2649897bd48bc3e44604bfa2d3bddc00c
itsyaya <yayarezai3@gmail.com>
2026-04-10 05:40:43 +0200

    feat: add exports for DashboardLayout and MainLayout components

--- frontend/src/hooks/useAsync.ts
+++ frontend/src/hooks/useAsync.ts
@@ -1,5 +1,4 @@
 import { useCallback, useState } from 'react';
-import toast from 'react-hot-toast';
 import { getErrorMessage } from '@/utils';
 
 interface UseAsyncState<T> {
---
+++ frontend/src/layouts/index.ts
@@ -0,0 +1,2 @@
+export { DashboardLayout } from './DashboardLayout';
+export { MainLayout } from './MainLayout';
